fbc96a96e3e0daf97fb441ff750527068a087c5c
hemanmehta <heman.mehta07@gmail.com>
2020-10-26 22:20:53 +0530

    Added Create n Save articles form with validations feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,31 @@ class ArticlesController < ApplicationController
 
     def index
         @articles = Article.all
-        
     end
+
+    def new
+        #Initialize only once when blank form submitted and error messages printed.
+        @article = Article.new
+    end
+
+    def create
+        #To print the paremeters for debugging in browser.
+        #render plain: params[:article]
+
+        #Whitelisting parameters from request aka Strong Parameters
+        #If we dont whitelist parameters then it will show ForbiddenAttributeError
+        #@article = Article.new(params.require(:TOP_LEVEL_KEY).permit(:KEY1, :KEY2))
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        # render plain: @article.inspect
+        
+        if @article.save
+            flash[:notice] = "Article was created Successfully."
+            #redirecting page to show article details
+            #redirect_to article_path(@article)
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   #To display specific routes for article we can use only option.
   #resource :article_name only: [:resource_attr_name]
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create]
 end
