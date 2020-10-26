442a40f594b56ec267ad796383e1631497ac8739
hemanmehta <heman.mehta07@gmail.com>
2020-10-26 18:48:13 +0530

    Added show feature for article resource

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,9 @@
+class ArticlesController < ApplicationController
+    def show
+        #Creating instance variable @article to fetch article based on index.
+        #[:id] will reflect index passed from URL
+        #Enable byebug for debugging and observe WebServer running console
+        #byebug
+        @article = Article.find(params[:id])
+    end
+end
