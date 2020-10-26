18d6e2acff106185a9bc0334fba6598cc2154764
hemanmehta <heman.mehta07@gmail.com>
2020-10-26 19:11:51 +0530

    Added Articles Listing feature for article resource

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,9 @@ class ArticlesController < ApplicationController
         #byebug
         @article = Article.find(params[:id])
     end
+
+    def index
+        @articles = Article.all
+        
+    end
 end
