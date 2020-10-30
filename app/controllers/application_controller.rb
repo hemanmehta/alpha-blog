3a008a76d3b3c9a4d360426954b2b2c1b5f6ee6f
hemanmehta <heman.mehta07@gmail.com>
2020-10-30 17:22:25 +0530

    Add auth methods to controller as helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+    #Whatever method defined in application controller will be available to all controllers of the app.
+   
+    #Designating current_user method as helper method so that it will be available in view component.
+    helper_method :current_user, :logged_in?
+    
+    def current_user
+        #Memorization, when method called every time it will query the database. Instead if we already have current 
+        #user information then it will directly return using instance variable w/o querying database.
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+        #Making boolean argument
+        !!current_user
+      end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,16 +6,4 @@ module ApplicationHelper
       gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
       image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
     end
-
-    def current_user
-      #Memorization, when method called every time it will query the database. Instead if we already have current 
-      #user information then it will directly return using instance variable w/o querying database.
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
-      
-    end
-
-    def logged_in?
-      #Making boolean argument
-      !!current_user
-    end
 end
